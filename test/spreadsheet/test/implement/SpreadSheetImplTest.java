ecdcc73ada853fc16dd042b4b1520c1c2bc595df
zhaow <zwy100e72@gmail.com>
2016-01-01 17:35:49 +0800

    Change: Rewrite setExpression and update for CellImpl

---
+++ test/spreadsheet/test/implement/SpreadSheetImplTest.java
@@ -0,0 +1,66 @@
+package spreadsheet.test.implement;
+
+import org.junit.Assert;
+import org.junit.Test;
+import spreadsheet.api.cell.Location;
+import spreadsheet.implement.CellImpl;
+import spreadsheet.implement.SpreadsheetImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by zhaow on 1/1/2016.
+ * SimpleExcel
+ */
+public class SpreadSheetImplTest {
+    @Test
+    public void testGetCellAt() {
+        Location location = new Location(1, 1);
+        SpreadsheetImpl spreadsheet = new SpreadsheetImpl();
+        CellImpl cell = spreadsheet.getCellAt(location);
+        Assert.assertNotNull(cell);
+        Assert.assertEquals(cell, spreadsheet.getCellAt(location));
+    }
+
+    @Test
+    public void testGetReferredLocation() {
+        String exp = "a1=a2";
+        Set<Location> target = SpreadsheetImpl.getReferredLocation(exp);
+        Set<Location> expect = new HashSet<>();
+        expect.add(new Location("a1"));
+        expect.add(new Location("a2"));
+        Assert.assertEquals(expect, target);
+    }
+
+    @Test
+    public void testGetReferredLocationMultipleReference() {
+        String exp = "a1=a2;a2=3";
+        Set<Location> target = SpreadsheetImpl.getReferredLocation(exp);
+        Set<Location> expect = new HashSet<>();
+        expect.add(new Location("a1"));
+        expect.add(new Location("a2"));
+        Assert.assertEquals(expect, target);
+    }
+
+    @Test
+    public void testGetReferredLocationLoopReference() {
+        String exp = "a1=a1";
+        Set<Location> target = SpreadsheetImpl.getReferredLocation(exp);
+        Set<Location> expect = new HashSet<>();
+        expect.add(new Location("a1"));
+        Assert.assertEquals(expect, target);
+    }
+
+    @Test
+    public void testGetChildren() {
+        SpreadsheetImpl spreadsheet = new SpreadsheetImpl();
+        CellImpl cell = new CellImpl(spreadsheet, null);
+        cell.setExpression("a2+a3");
+        Set<CellImpl> target = spreadsheet.getChildren(cell);
+        Set<CellImpl> expect = new HashSet<>();
+        expect.add(new CellImpl(spreadsheet, new Location("a2")));
+        expect.add(new CellImpl(spreadsheet, new Location("a3")));
+        Assert.assertEquals(expect, target);
+    }
+}
