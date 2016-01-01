502c916a747c7083e7a05e53d57fd265f86fd776
zhaow <zwy100e72@gmail.com>
2016-01-01 17:36:40 +0800

    Change: Rewrite CellImpl setExpression and update (2)

--- src/spreadsheet/implement/CellImpl.java
+++ src/spreadsheet/implement/CellImpl.java
@@ -3,6 +3,7 @@ package spreadsheet.implement;
 import spreadsheet.api.cell.Cell;
 import spreadsheet.api.cell.Location;
 import spreadsheet.api.value.Value;
+import spreadsheet.api.value.vInvalid;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,10 +19,10 @@ public class CellImpl implements Cell {
     private String expression;
     private boolean isModified;
 
-    private Set<CellImpl> referTo = new HashSet<>();
-    private Set<Cell> referFrom = new HashSet<>();
+    private Set<CellImpl> thisRefer = new HashSet<>();
+    private Set<Cell> referThis = new HashSet<>();
 
-    public CellImpl(SpreadsheetImpl spreadSheet, Location location){
+    public CellImpl(SpreadsheetImpl spreadSheet, Location location) {
         this.spreadSheet = spreadSheet;
         this.location = location;
         this.value = null;
@@ -29,32 +30,25 @@ public class CellImpl implements Cell {
         this.isModified = false;
     }
 
-    public Set<CellImpl> getReferTo() {
-        return referTo;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CellImpl cell = (CellImpl) o;
+
+        return spreadSheet != null ? spreadSheet.equals(cell.spreadSheet) : cell.spreadSheet == null &&
+                (location != null ? location.equals(cell.location) : cell.location == null &&
+                        (expression != null ? expression.equals(cell.expression) : cell.expression == null));
+
     }
 
-    public void setReferTo(Set<CellImpl> referTo) {
-        this.referTo = referTo;
-    }
-
-    public Set<Cell> getReferFrom() {
-        return referFrom;
-    }
-
-    public void setReferFrom(Set<Cell> referFrom) {
-        this.referFrom = referFrom;
-    }
-
-    public boolean isModified() {
-        return isModified;
-    }
-
-    public void setModified(boolean modified) {
-        isModified = modified;
-    }
-
-    public Location getLocation() {
-        return location;
+    @Override
+    public int hashCode() {
+        int result = spreadSheet != null ? spreadSheet.hashCode() : 0;
+        result = 31 * result + (location != null ? location.hashCode() : 0);
+        result = 31 * result + (expression != null ? expression.hashCode() : 0);
+        return result;
     }
 
     public Value getValue() {
@@ -70,11 +64,39 @@ public class CellImpl implements Cell {
     }
 
     public void setExpression(String expression) {
+        for (CellImpl cell : thisRefer) {
+            cell.referThis.remove(this);
+        }
+        this.thisRefer.clear();
+
         this.expression = expression;
+
+        setValue(new vInvalid(expression));
+
+        for (Location location : SpreadsheetImpl.getReferredLocation(expression)) {
+            CellImpl cell = spreadSheet.getCellAt(location);
+            thisRefer.add(cell);
+            cell.referThis.add(this);
+        }
+
+        for (Cell cell : referThis)
+            cell.update(this);
     }
 
     @Override
     public void update(Cell changed) {
+        if (!spreadSheet.getModified().contains(this)) {
+            spreadSheet.getModified().add(this);
+            setValue(new vInvalid(expression));
 
+            for (Cell cell : referThis)
+                cell.update(this);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s->%s)", location, expression);
     }
 }
--- src/spreadsheet/implement/SpreadsheetImpl.java
+++ src/spreadsheet/implement/SpreadsheetImpl.java
@@ -3,26 +3,57 @@ package spreadsheet.implement;
 import spreadsheet.api.SpreadSheet;
 import spreadsheet.api.cell.Location;
 import spreadsheet.api.value.Value;
+import spreadsheet.api.value.vInvalid;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Created by zhaow on 12/30/2015.
  * SimpleExcel
  */
-public class SpreadsheetImpl implements SpreadSheet{
+public class SpreadsheetImpl implements SpreadSheet {
+    private Map<Location, CellImpl> locationCellMap = new HashMap<>();
+    private Set<CellImpl> modified = new HashSet<>();
+    private Set<CellImpl> ignore = new HashSet<>();
 
-    private final int colCount;
-    private final int rowCount;
-
-    public SpreadsheetImpl(int rowCount, int colCount){
-        this.rowCount = rowCount;
-        this.colCount = colCount;
-
+    public CellImpl getCellAt(Location location) {
+        CellImpl cell = locationCellMap.get(location);
+        if (cell == null) {
+            cell = new CellImpl(this, location);
+            cell.setValue(null);
+            locationCellMap.put(location, cell);
+        }
+        return cell;
     }
 
+    public static Set<Location> getReferredLocation(String exp) {
+        Set<Location> result = new HashSet<>();
+        Pattern p = Pattern.compile("[a-zA-z]+[1-9]\\d*");
+        Matcher m = p.matcher(exp);
+        while (m.find()) {
+            String location = exp.substring(m.start(), m.end());
+            result.add(new Location(location));
+        }
+        return result;
+    }
+
+    public Set<CellImpl> getChildren(CellImpl cell) {
+        Set<Location> childrenLocation = getReferredLocation(cell.getExpression());
+        return childrenLocation.stream().map(this::getCellAt).collect(Collectors.toSet());
+    }
 
     @Override
     public void setExpression(Location location, String expression) {
-
+        CellImpl cell = getCellAt(location);
+        if (!modified.contains(cell) || cell.getExpression().equals(expression))
+            cell.setExpression(expression);
+        cell.setValue(new vInvalid(expression));
     }
 
     @Override
@@ -38,5 +69,9 @@ public class SpreadsheetImpl implements SpreadSheet{
     @Override
     public void reCompute() {
 
+    }
+
+    public Set<CellImpl> getModified() {
+        return modified;
     }
 }
